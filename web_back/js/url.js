7205c8adee623fc4dcff841be0d1785362fc3cdb
kangkang-github <56428932+kangkang-github@users.noreply.github.com>
2019-10-11 17:37:47 +0800

    后台登录

---
+++ web_back/js/url.js
@@ -0,0 +1,7 @@
+// 基地址
+var baseUrl = 'http://localhost:8000';
+
+// 后面接口
+var USER_LOGIN = baseUrl + '/admin/login';
+var USER_LOGOUT = baseUrl + '/admin/logout';
+var USER_GETINFO = baseUrl + '/admin/getuser';
